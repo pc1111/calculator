30aefbdfde2d42eb03a0a74e24d4173e19c00839
pc1111 <pc1111@kmu.ac.kr>
2018-04-12 15:07:12 +0900

    checkstyle

--- src/main/java/com/pc1111/calculator/CalculatorApplication.java
+++ src/main/java/com/pc1111/calculator/CalculatorApplication.java
@@ -3,7 +3,9 @@ package com.pc1111.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**aaa*/
 @SpringBootApplication
+/**aaaaa*/
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
