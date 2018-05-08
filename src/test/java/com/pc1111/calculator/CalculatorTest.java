8158ac245ff19e8f5f51f1c79d3d5f6756989009
pc1111 <pc1111@kmu.ac.kr>
2018-05-08 16:21:21 +0900

    CalculatorTest

--- src/test/java/com/pc1111/calculator/CalculatorTest.java
+++ src/test/java/com/pc1111/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.pc1111.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+/***/
 public class CalculatorTest {
 	private Calculator calculator = new Calculator();
 	
